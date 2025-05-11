755e5598f60613d032275837530566445cac0270
Abdullah Raed <abdullahraediti@gmail.com>
2025-05-11 22:03:45 +0300

    adding leagues table and custom cell

---
+++ Sportify/Home/View/Selectedleague.swift
@@ -0,0 +1,15 @@
+//
+//  Selectedleague.swift
+//  Sportify
+//
+//  Created by Abdullah Raed on 11/05/2025.
+//
+
+import Foundation
+
+enum Selectedleague: Int {
+    case football = 0
+    case basketball = 1
+    case cricket = 2
+    case tennis = 3
+}
